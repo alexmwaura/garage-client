624f61e5897d30863ae94202b96371a65a5a8e4d
alexmwaura <alexmwaura43@gmail.com>
2020-04-18 19:02:38 +0300

    add custom theme provider

---
+++ src/util/theme.js
@@ -0,0 +1,73 @@
+export default {
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#827717",
+      dark: "#ff9800",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff9800",
+      dark: "#827717",
+      contrastText: "#fff",
+    },
+  },
+
+  spreadThese: {
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: 'center',
+      // margin: '40px auto 40px auto'
+     
+      
+    },
+    image: {
+      margin: '10px auto 10px auto',
+      height:50,
+      borderRadius:100
+    },
+    pageTitle: {
+      margin: '10px auto 10px auto'
+    },
+  
+    customError: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginTop: 10
+    },
+    progress: {
+      position: 'absolute'
+    },
+    invisibleSeparator: {
+      border: 'none',
+      margin: 4
+    },
+    visibleSeparator: {
+      width: '100%',
+      borderBottom: '1px solid rgba(0,0,0,0.1)',
+      marginBottom: 20
+    },
+    paper: {
+      padding: 20
+    },
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    formControl: {
+      margin: 1,
+      minWidth: 120,
+    },
+  },
+
+};
