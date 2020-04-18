9068cdf9efee9c84095aa7d48b635edfebd05ef4
alexmwaura <alexmwaura43@gmail.com>
2020-04-18 19:03:16 +0300

    add main App file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeObject from "./util/theme";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Navbar from "./components/nav";
+import ExtraNav from "./components/extraNav";
+import AuthRoute from "./util/AuthRoute";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import signup from "./pages/signup";
+import login from "./pages/login";
+import attendant from "./pages/attendant";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { SET_AUTHENTICATED } from "./redux/types";
+import profile from "./components/userProfile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getAttendantData } from "./redux/actions/userActions";
+
+import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import AppIcon from "./images/calltronix.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
+
+const theme = createMuiTheme(themeObject);
+
+class App extends Component {
+  componentDidMount() {
+    const token = localStorage.FBIdToken;
+    const username = localStorage.username;
+
+
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.clear();
+        // store.dispatch(logoutUser());
+        window.location.href = "/";
+      } else {
+        store.dispatch({ type: SET_AUTHENTICATED });
+        axios.defaults.headers.common["Authorization"] = token;
+        store.dispatch(getAttendantData(username));
+      }
+    }
+  }
+
+  render() {
+    // const classes = useStyles();
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <div className="wrapper ">
+            <Navbar />
+            <ExtraNav />
+          </div>
+          <Router>
+            <Switch>
+              <Route exact path="/attendant" component={attendant} />
+              <Route exact path="/profile" component={profile} />
+              <AuthRoute exact path="/" component={login} />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
+            </Switch>
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
 }
-
 export default App;
