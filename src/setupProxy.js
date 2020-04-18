40c8cf01a06eec11d1e6c1759c9c2ba882422ea6
alexmwaura <alexmwaura43@gmail.com>
2020-04-18 18:50:43 +0300

    add custom proxy middleware for api endpoint

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+        target: 'https://us-central1-auto-garage-ea474.cloudfunctions.net',
+        logLevel: 'debug',
+        changeOrigin: true
+    })
+  );
+};
